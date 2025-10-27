cd7933cb3421ebb950cce56ed99d105e5cb24007
b01tech <bruno01.tech@gmail.com>
2025-10-26 21:56:51 -0300

    feat(api): add OpenAI service extension for dependency injection

---
+++ MentorIA.API/Extensions/OpenAIExtensions.cs
@@ -0,0 +1,22 @@
+﻿using OpenAI;
+
+namespace MentorIA.API.Extensions;
+
+public static class OpenAIExtensions
+{
+    public static IServiceCollection AddOpenAI(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        var apiKey = configuration["OpenIA:ApiKey"];
+        if (string.IsNullOrEmpty(apiKey))
+        {
+            throw new InvalidOperationException("OpenAI: API Key is missing");
+        }
+
+        var openIAClient = new OpenAIClient(apiKey);
+        services.AddSingleton(openIAClient);
+        return services;
+    }
+}
--- MentorIA.API/Program.cs
+++ MentorIA.API/Program.cs
@@ -3,7 +3,8 @@ using MentorIA.API.Extensions;
 DotNetEnv.Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDocumentationApi();
+
+builder.Services.AddDocumentationApi().AddOpenAI(builder.Configuration);
 
 var app = builder.Build();
 
