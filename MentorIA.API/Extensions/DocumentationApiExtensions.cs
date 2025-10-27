5c8fdab3e002a9bb0db88b97cc0e396d7e64218a
b01tech <bruno01.tech@gmail.com>
2025-10-26 21:23:00 -0300

    feat: add `scalar` to documentation api

---
+++ MentorIA.API/Extensions/DocumentationApiExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.OpenApi.Models;
+using Scalar.AspNetCore;
+
+namespace MentorIA.API.Extensions;
+
+public static class DocumentationApiExtensions
+{
+    public static IServiceCollection AddDocumentationApi(this IServiceCollection services)
+    {
+        services.AddOpenApi("v1", opt =>
+        {
+            opt.AddDocumentTransformer((doc, context, cancellationToken) =>
+            {
+                doc.Info = new OpenApiInfo
+                {
+                    Title = "Mentor AI API",
+                    Version = "v1",
+                    Description = "API de comunicação com Open AI",
+                };
+                return Task.CompletedTask;
+            });
+        });
+        return services;
+    }
+
+    public static void UseDocumentationApi(this WebApplication app)
+    {
+        app.MapOpenApi();
+        app.MapScalarApiReference(opt =>
+        {
+            opt.Title = "Mentor AI API";
+            opt.Theme = ScalarTheme.Solarized;
+            opt.DarkMode = true;
+        });
+    }
+}
--- MentorIA.API/Program.cs
+++ MentorIA.API/Program.cs
@@ -1,9 +1,13 @@
+using MentorIA.API.Extensions;
+
 DotNetEnv.Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDocumentationApi();
 
 var app = builder.Build();
 
+app.UseDocumentationApi();
 app.UseHttpsRedirection();
 
 app.Run();
